2dc4724212bb3c849a2db2b890187e4bc5dc81a1
Himanshu sharma <hs991009@gmail.com>
2023-10-10 17:40:33 +0530

    changes

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import routes from './routes/route.js';
 import Connection  from './db/conn.js';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+const port=process.env.PORT || 8080;
 const app=express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json({extended:true}));
 app.use('/',routes);
 Connection();
-app.listen(8080,()=>{
+app.listen(port,()=>{
     console.log("server is running on port 8080 ");
 })
